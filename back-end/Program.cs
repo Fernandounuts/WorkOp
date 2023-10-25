d4abd20a76ee22c4b226d0b17c26142a30e01b5b
Fernandounuts <fernando.balieiro0580@hotmail.com>
2023-10-25 16:10:56 -0300

    adicionado connection string para o banco de dados docker

--- back-end/Program.cs
+++ back-end/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<back_end.Infra.AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DbString"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
