4158a64416b38059a21b2eeaf674b31652001070
Fernandounuts <fernando.balieiro0580@hotmail.com>
2023-10-25 22:55:22 -0300

    repository da competencia terminado

---
+++ back-end/Repository/CompetenciaRepository.cs
@@ -0,0 +1,54 @@
+using back_end.Infra;
+using back_end.Interfaces;
+using back_end.Models.UsuarioModel;
+
+namespace back_end.Repository;
+
+public class CompetenciaRepository : ICompetenciaRepositoryInterface
+{
+    private readonly AppDbContext _context;
+
+    public CompetenciaRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> PostCompetencia(long? id, Competencia competencia)
+    {
+        if (id != null)
+        {
+            competencia.UsuarioId = id;
+        }
+
+        await _context.AddAsync(competencia);
+        return await Save();
+    }
+
+    public async Task<bool> UpdateCompetencia(long? id, Competencia competencia)
+    {
+        if (id != null)
+        {
+            competencia.UsuarioId = id;
+        }
+
+        _context.Update(competencia);
+        return await Save();
+
+    }
+
+    public async Task<bool> DeleteCompetencia(long? id, Competencia competencia)
+    {
+        if (id != null)
+        {
+            competencia.UsuarioId = id;
+        }
+
+        _context.Remove(competencia);
+        return await Save();
+    }
+
+    public async Task<bool> Save()
+    {
+        return await _context.SaveChangesAsync() > 0;
+    }
+}
