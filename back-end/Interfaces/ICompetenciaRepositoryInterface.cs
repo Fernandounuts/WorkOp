b1c9b85b9fe0bfa953fc1562111449d1d23b2b2f
Fernandounuts <fernando.balieiro0580@hotmail.com>
2023-10-25 22:55:09 -0300

    ICompetenciaRepository completo

--- back-end/Interfaces/ICompetenciaRepositoryInterface.cs
+++ back-end/Interfaces/ICompetenciaRepositoryInterface.cs
@@ -1,6 +1,11 @@
+using back_end.Models.UsuarioModel;
+
 namespace back_end.Interfaces;
 
 public interface ICompetenciaRepositoryInterface
 {
-    
+    Task<bool> PostCompetencia(long? id, Competencia competencia);
+    Task<bool> UpdateCompetencia(long? id, Competencia competencia);
+    Task<bool> DeleteCompetencia(long? id, Competencia competencia);
+    Task<bool> Save();
 }
